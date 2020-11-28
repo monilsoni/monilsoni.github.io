debeb0a6f2d53470b8abe4f355a6b377e58aa553
Monil Soni <monisoni@amazon.com>
2020-11-28 21:09:05 +0530

    Setup bootstrap for Vuejs

--- vue-migration/src/views/LandingPage.vue
+++ vue-migration/src/views/LandingPage.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="container-fluid">
-        <div class="row heading heading-color-scheme text-center">
-            <div class="col" style="margin: auto;">
-                <h1>I'm Monil</h1>
-                <h2>A Software Development Engineer</h2>
-            </div>
-        </div>
+  <div class="container-fluid">
+    <div class="row heading heading-color-scheme text-center">
+      <div class="col" style="margin: auto;">
+        <h1>I'm Monil</h1>
+        <h2>A Software Development Engineer</h2>
+      </div>
     </div>
+  </div>
 </template>
