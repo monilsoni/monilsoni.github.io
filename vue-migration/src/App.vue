8e693ab6c42fac255873aeb91475bf6f3346b749
Monil Soni <monisoni@amazon.com>
2020-11-29 22:27:41 +0530

    Partially fix navbar

--- vue-migration/src/App.vue
+++ vue-migration/src/App.vue
@@ -1,64 +1,36 @@
 <template>
   <div id="app">
-    <nav
-      class="navbar sticky-top navbar-expand-md"
-      role="navigation"
-    >
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item mx-4">
-            <router-link to="/">Home</router-link>
-          </li>
-          <li class="nav-item mx-4">
+    <b-navbar toggleable="sm" type="dark" class="navbar" sticky>
+      <b-navbar-toggle target="navbar-toggle-collapse"></b-navbar-toggle>
+      <b-collapse id="navbar-toggle-collapse" is-nav>
+        <b-navbar-nav class="mx-auto navbar-nav" fill>
+          <b-nav-item class="mx-4"> 
+            <router-link to="/">Home</router-link> 
+          </b-nav-item>
+          <b-nav-item class="mx-4">
             <router-link to="/about">About Me</router-link>
-          </li>
-          <li class="nav-item mx-4">
+          </b-nav-item>
+          <b-nav-item class="mx-4">
             <router-link to="/work">Work</router-link>
-          </li>
-          <li class="nav-item mx-4">
+          </b-nav-item>
+          <b-nav-item class="mx-4">
             <router-link to="/contact">Contact Me</router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view />
   </div>
 </template>
 
 <style scoped>
-
 .navbar {
   background-color: #34495e;
   color: #f4f7f7;
 }
 
 .navbar-toggler-icon {
-    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255, 255, 255, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
-}
-
-@media (min-width: 768px) {
-    .navbar-nav {
-        float: none;
-        margin: 0 auto;
-        display: block;
-        text-align: center;
-      }
-      
-      .navbar-nav > li {
-        display: inline-block;
-        float: none;
-      }
+  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255, 255, 255, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
 }
 
 .navbar-nav a {
@@ -67,8 +39,11 @@
 }
 
 .navbar-nav a:hover {
-  color: #f4f7f7;
-  text-decoration: underline;
+  color: #aacfd0;
 }
 
+.navbar a.router-link-exact-active {
+  color: #5da0a2;
+  font-weight: bold;
+}
 </style>
